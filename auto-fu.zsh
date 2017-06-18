20d40c29a6b26b6a662323e5123d340014a31526
halide <>
2017-06-18 01:47:26 -0500

    history completion fixed

--- auto-fu.zsh
+++ auto-fu.zsh
@@ -5,9 +5,9 @@ afu_zles=( \
   kill-whole-line kill-word magic-space yank \
 )
 
+# Keybinds. may conflict with other behaviors
 afu-install () {
   bindkey -N afu emacs
-  #{ "$@" }
   bindkey -M afu "^I" afu+expand-or-complete
   bindkey -M afu "^M" afu+accept-line
 
@@ -20,7 +20,6 @@ afu-install () {
   bindkey -M afu "^[[B" down-line-or-beginning-search # Down
 }
 
-afu-install #afu-keymap+widget
 
 afu-register-zle-accept-line () {
   local afufun="$1"
@@ -42,17 +41,15 @@ EOT
   ))"}
   eval "${${code//\$afufun/$afufun}//\$rawzle/$rawzle}"
   afu_accept_lines+=$afufun
-  BUFFER=''
 }
 
-#irrelevant solely for printing lines
 afu-register-zle-expand-or-complete () {
   local afufun="$1"
   local rawzle=".${afufun#*+}"
   local code=${"$(<=(cat <<"EOT"
   $afufun () {
     if [[ $afu_one_match_p != t ]]; then
-        buffer_cur="$BUFFER"
+      buffer_cur="$BUFFER"
     fi
     inTyping=0
     zle $rawzle
@@ -63,36 +60,32 @@ EOT
   ))"}
   eval "${${code//\$afufun/$afufun}//\$rawzle/$rawzle}"
 }
-afu-register-zle-accept-line afu+accept-line
-afu-register-zle-expand-or-complete afu+expand-or-complete
 
 # Entry point.
 auto-fu-init () {
-    zle_highlight=(default:"fg=white")
+  zle_highlight=(default:"fg=white")
   local auto_fu_init_p=1
   local ps
   {
     local afu_in_p=0
     local afu_paused_p=0
-
     zstyle -s ':auto-fu:var' postdisplay ps
     [[ -z ${ps} ]] || POSTDISPLAY="$ps"
-
     afu-recursive-edit-and-accept
     zle -I
   } always {
     [[ -z ${ps} ]] || POSTDISPLAY=''
   }
 }
-zle -N auto-fu-init
 
 afu-recursive-edit-and-accept () {
   local -a __accepted
-    region_highlight=("${#buffer_cur} ${#buffer_new} fg=white")
+  region_highlight=("${#buffer_cur} ${#buffer_new} fg=white")
   zle recursive-edit -K afu || { zle -R ''; zle send-break; return }
   [[ -n ${__accepted} ]] &&
   (( ${#${(M)afu_accept_lines:#${__accepted[1]}}} > 1 )) &&
   { zle "${__accepted[@]}"} || { zle accept-line }
+      
 }
 
 afu-clearing-maybe () {
@@ -109,17 +102,17 @@ with-afu () {
     afu-clearing-maybe
     ((afu_in_p == 1)) && { afu_in_p=0; BUFFER="$buffer_cur" }
     zle $zlefun && {
-        setopt localoptions extendedglob no_banghist
-        #region_highlight=("P0 ${#buffer_cur} fg=green")
-        local es ds
-        zstyle -a ':auto-fu:var' enable es; (( ${#es} == 0 )) && es=(all)
-        if [[ -n ${(M)es:#(#i)all} ]]; then
-            zstyle -a ':auto-fu:var' disable ds
-            : ${(A)es::=${zs:#(${~${(j.|.)ds}})}}
-        fi
-        [[ -n ${(M)es:#${zlefun#.}} ]]
+      setopt localoptions extendedglob no_banghist
+      #region_highlight=("P0 ${#buffer_cur} fg=green")
+      local es ds
+      zstyle -a ':auto-fu:var' enable es; (( ${#es} == 0 )) && es=(all)
+      if [[ -n ${(M)es:#(#i)all} ]]; then
+        zstyle -a ':auto-fu:var' disable ds
+        : ${(A)es::=${zs:#(${~${(j.|.)ds}})}}
+      fi
+      [[ -n ${(M)es:#${zlefun#.}} ]]
     } && {
-        auto-fu-maybe
+      auto-fu-maybe
     }
 }
 
@@ -135,7 +128,6 @@ afu-initialize-zle-afu () {
     afu-register-zle-afu $z
   done
 }
-afu-initialize-zle-afu
 
 auto-fu-maybe () {
   (($PENDING== 0)) && [[ $LBUFFER != *$'\012'*  ]] &&
@@ -163,6 +155,7 @@ auto-fu () {
     }
 
     inTyping=1
+
     if [[ "$buffer_cur" != "$buffer_new" ]] || ((cursor_cur != cursor_new))
     then afu_in_p=1; {
       local BUFFER="$buffer_cur"
@@ -193,9 +186,13 @@ afu-comppost () {
     compstate[list]=''
     zle -M "$compstate[list_lines]($compstate[nmatches]) too many matches..."
   }
-
   typeset -g afu_one_match_p=
   (( $compstate[nmatches] == 1 )) && afu_one_match_p=t
 }
-zle -N auto-fu
 
+afu-install
+afu-register-zle-accept-line afu+accept-line
+afu-register-zle-expand-or-complete afu+expand-or-complete
+zle -N auto-fu-init
+afu-initialize-zle-afu
+zle -N auto-fu
