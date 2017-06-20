17aab0f28b2ed99a9454f5e6ba69d67c0bb729b7
halide <>
2017-06-20 12:10:53 -0500

    rm source-test.zsh add new .zshrc

--- source-test.zsh
+++
@@ -1,9 +0,0 @@
-source $HOME/.zsh/auto-fu.zsh/auto-fu.zsh
-zstyle ':completion:*:default'         list-colors ${(s.:.)LS_COLORS}
-zstyle ':completion:*:descriptions'    format $'%{\e[0;31m%}completing %B%d%b%{\e[0m%}'                                                             
-zstyle ':completion:*' special-dirs ..
-function zle-line-init () {
-   auto-fu-init
-   zle reset-prompt
-}
-zle -N zle-line-init
